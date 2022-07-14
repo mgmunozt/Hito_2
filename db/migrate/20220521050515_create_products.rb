7f0c80bef27872073c5298b48e6bede5aabfccbb
Mario Munoz <mariog.munoztellez@gmail.com>
2022-07-13 21:23:33 -0400

    commithito2a

--- db/migrate/20220521050515_create_products.rb
+++ db/migrate/20220521050515_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :code
       t.string :description
-      t.string :salesUnit
+      t.string :salesUnits
 
       t.timestamps
     end
